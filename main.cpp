48a6dd223efc5b7129b0fe20b3105e3a704e5074
Rosetta Roberts <rosettafroberts@gmail.com>
2018-09-11 09:38:24 -0600

    Added documentation and implemented bubblesort.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <vector>
 #include <memory>
+#include <ctime>
 #include "bubbleSort.h"
 #include "selectionSort.h"
 #include "mergeSort.h"
@@ -12,16 +13,20 @@ unique_ptr<int[]> readFile(const string &fileName, size_t &length);
 
 void printUsage(char *programName);
 
+/**
+ * Opens the file at the first argument and
+ * then sorts the numbers in it by the sort
+ * specified with the second argument.
+ */
 int main(int argc, char *argv[]) {
     if (argc < 3) {
         printUsage(argv[0]);
         return 1;
     }
-
-    long sortType = strtol(argv[2], nullptr, 10);
-
     size_t length = 0;
     unique_ptr<int[]> array = readFile(argv[1], length);
+
+    long sortType = strtol(argv[2], nullptr, 10);
 
     clock_t start = clock();
 
@@ -43,7 +48,7 @@ int main(int argc, char *argv[]) {
 
     clock_t end = clock();
 
-    double milliseconds = double(end - start) / CLOCKS_PER_SEC / 1000;
+    double milliseconds = double(end - start) / CLOCKS_PER_SEC * 1000;
 
     for (size_t i = 0; i < length; i++)
     {
@@ -55,18 +60,33 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
+/**
+ * Reads a file from fileName into an array.
+ * The file must start with an integer saying how many other
+ * integers are in the file. Each integer is then specified on
+ * its own line in the file. e.g.:
+ * 3
+ * 123
+ * -4
+ * 42
+ * @param fileName the name of the file to read from.
+ * @param length populated with the number of integers returned.
+ * @return the number of integers in the file except.
+ * for the first one specifying the number of integers.
+ */
 unique_ptr<int[]> readFile(const string &fileName, size_t &length) {
     ifstream file(fileName);
 
     file >> length;
 
     //Using unique pointer instead of raw pointer
-    //so that it would automatically be destroyed
-    //when no longer being used
-    auto array = std::make_unique<int[]>(static_cast<size_t>(length));
+    //so RAII deletes the pointer when no longer in use
+    //rather than me having to worry about it and
+    //be paranoid about exceptions evading delete statements.
+    auto array = std::make_unique<int[]>(length);
     size_t i = 0;
 
-    while (!file.eof()) {
+    while (!file.eof() && i < length) {
         file >> array[i++];
     }
     file.close();
@@ -74,6 +94,10 @@ unique_ptr<int[]> readFile(const string &fileName, size_t &length) {
     return array;
 }
 
+/**
+ * Prints how to use this program to stdout.
+ * @param programName the name of the program.
+ */
 void printUsage(char *programName) {
     printf("Usage: %s filename sortToUse\n", programName);
     printf("  sortToUse (input a listed number):\n");
