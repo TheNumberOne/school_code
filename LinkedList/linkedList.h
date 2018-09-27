518e67acf2b7674dd7b4db60693b59a741291bf6
Rosetta Roberts <rosettafroberts@gmail.com>
2018-09-26 21:02:03 -0600

    added definitions

--- LinkedList/linkedList.h
+++ LinkedList/linkedList.h
@@ -28,5 +28,35 @@ class linked_list
 
 public:
 	linked_list(): first_(nullptr), last_(nullptr) {}
+	~linked_list();
 	linked_list(const linked_list<T> &other);
+	linked_list(linked_list<T> &&other);
+	linked_list<T>& operator=(const linked_list<T> &other);
+	linked_list<T>& operator=(linked_list<T> &&other);
+	void swap(linked_list<T> &other);
+
+	void insert_beginning(node<T>* node);
+	node<T>* insert_beginning(const T &item);
+	node<T>* insert_beginning(T &&item);
+
+	void insert_end(node<T>* node);
+	node<T>* insert_end(const T &item);
+	node<T>* insert_end(T &&item);
+
+	void insert_after(node<T>* node, node<T>* next);
+	node<T>* insert_after(node<T>* node, const T &item);
+	node<T>* insert_after(node<T>* node, T &&item);
+
+	void insert_before(node<T>* node, node<T>* before);
+	node<T>* insert_before(node<T>* node, const T &item);
+	node<T>* insert_before(node<T>* node, T &&item);
+
+	void remove(node<T>* node);
+	void append(const linked_list<T> &other);
+
+	void swap(node<T>* left, node<T>* right);
+
+	node<T>* get_node(int index);
+	const node<T>* get_node(int index) const;
+
 };
