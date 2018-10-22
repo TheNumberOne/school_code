fefd9d0be0f45bc21ae8c3883af81c4265e29fe5
Rosetta Roberts <rosettafroberts@gmail.com>
2018-10-21 21:51:29 -0600

    Added minimum stuff for calc_gcd

--- calc_gcd.s
+++ calc_gcd.s
@@ -4,10 +4,8 @@
 	.text
 	.align	2
 main:
-	str	fp, [sp, #-4]!
-	add	fp, sp, #0
-	mov	r3, #0
-	mov	r0, r3
-	add	sp, fp, #0
-	ldr	fp, [sp], #4
-	bx	lr
+	push	{fp, lr}   @ push return address and old frame pointer onto the stack
+	add	fp, sp, 4  @ move frame pointer to point to the first variable put onto the stack by this function
+	
+	add	sp, fp, -4 @ move stack pointer so only the first two variables on the stack are there.
+	pop	{fp, pc}   @ restore the old frame pointer and jump to the return address
