31256271d566089d60af59e4a2f0b260c963d5f5
Rosetta Roberts <rosettafroberts@gmail.com>
2019-01-31 11:09:08 -0700

    Partially implemented heap

--- p157/Heap.cpp
+++ p157/Heap.cpp
@@ -1,89 +1,123 @@
 #include "./Heap.h"
-#include <iostream>
+
 #include <sstream>
 // Provides floor, ceil, etc.
 #include <cmath>
 
 using namespace std;
 
-Heap::Heap() {
-  arraySize = 0;
-  n = 0;
-  A = NULL;
-}
+Heap::Heap() : A(0), arraySize(0), n(0) {}
 
 // This assumes that every element of the array is an
 // element of the heap.
-Heap::Heap(int* inArray, int inArraySize, int inHeapSize) {
-  // TODO: initialize your class data members. An array dynamically allocated
-  // as follows:
-  // A = new int[size];
-  // If you allocate an array like this you MUST deallocate it in your
-  // destructor. This is done for you in the destructor below.
-}
-
-// Destructor. Cleans up memory.
-Heap::~Heap() {
-  delete [] A;
+Heap::Heap(int *inArray, int inArraySize, int inHeapSize) :
+        A(static_cast<size_t>(inHeapSize)),
+        arraySize(inArraySize),
+        n(0) {
+    std::copy(inArray, inArray + inArraySize, A.val);
 }
 
 // Note: the function name is prefixed by Heap:: (the class
 // name followed by two colons). Any function defined in
 // the .cpp file must have this prefix.
 int Heap::at(int i) const {
-  return A[i];
+    return A.val[i];
 }
 
-bool Heap::operator==(const Heap& rhs) {
-  if (n != rhs.n) return false;
-  for (int i = 0; i < n; ++i) {
-    if (A[i] != rhs.A[i]) return false;
-  }
-  return true;
-}
-
-bool Heap::operator==(const int* rhs) {
-  for (int i = 0; i < n; ++i) {
-    if (A[i] != rhs[i]) return false;
-  }
-  return true;
-}
-
-std::ostream& operator<<(std::ostream& out, const Heap& h) {
-  out << "[";
-  for (int i = 0; i < h.n; ++i) {
-    out << h.A[i];
-    if (i < h.n-1) {
-      out << ", ";
+bool Heap::operator==(const Heap &rhs) {
+    if (n != rhs.n) return false;
+    for (int i = 0; i < n; ++i) {
+        if (A.val[i] != rhs.A.val[i]) return false;
     }
-  }
-  out << "]";
-  return out;
+    return true;
 }
 
-string toDotImpl(const Heap& h, int i) {
-  using namespace std;
-  stringstream ss;
-  if (h.hasLeft(i)) {
-    ss << toDotImpl(h, h.left(i));
-    ss << "\"" << h.at(i) << "\" -> \""
-       << h.at(h.left(i)) << "\"\n";
-  }
-  if (h.hasRight(i)) {
-    ss << toDotImpl(h, h.right(i));
-    ss << "\"" << h.at(i) << "\" -> \""
-       << h.at(h.right(i)) << "\"\n";
-  }
-  return ss.str();
+bool Heap::operator==(const int *rhs) {
+    for (int i = 0; i < n; ++i) {
+        if (A.val[i] != rhs[i]) return false;
+    }
+    return true;
 }
 
-string toDot(const Heap& h) {
-  using namespace std;
-  stringstream ss;
-  ss << "digraph G {\n";
-  ss << "graph [ordering=\"out\"]\n";
-  ss << "\"" << h.at(0) << "\"\n";
-  ss << toDotImpl(h, 0);
-  ss << "}\n";
-  return ss.str();
+std::ostream &operator<<(std::ostream &out, const Heap &h) {
+    out << "[";
+    for (int i = 0; i < h.n; ++i) {
+        out << h.A.val[i];
+        if (i < h.n - 1) {
+            out << ", ";
+        }
+    }
+    out << "]";
+    return out;
+}
+
+int Heap::left(int i) const {
+    if (i < 0) return -1;
+    int left = i * 2 + 1;
+    return left >= n ? -1 : left;
+}
+
+int Heap::right(int i) const {
+    if (i < 0) return -1;
+    int right = i * 2 + 2;
+    return right >= n ? -1 : right;
+}
+
+int Heap::parent(int i) const {
+    if (i >= n || i < 0) return -1;
+    return (i - 1) / 2;
+}
+
+void Heap::maxHeapify(int i) {
+    while (true)
+    {
+        int l = left(i);
+        int r = right(i);
+        int max = i;
+        if (l != -1 && A[l] > A[max]) max = l;
+        if (r != -1 && A[r] > A[max]) max = r;
+        if (max == i) return;
+        using std::swap;
+        swap(A[i], A[max]);
+        i = max;
+    }
+}
+
+void Heap::buildMaxHeap() {
+
+}
+
+bool Heap::hasLeft(int i) const {
+    return false;
+}
+
+bool Heap::hasRight(int i) const {
+    return false;
+}
+
+string toDotImpl(const Heap &h, int i) {
+    using namespace std;
+    stringstream ss;
+    if (h.hasLeft(i)) {
+        ss << toDotImpl(h, h.left(i));
+        ss << "\"" << h.at(i) << "\" -> \""
+           << h.at(h.left(i)) << "\"\n";
+    }
+    if (h.hasRight(i)) {
+        ss << toDotImpl(h, h.right(i));
+        ss << "\"" << h.at(i) << "\" -> \""
+           << h.at(h.right(i)) << "\"\n";
+    }
+    return ss.str();
+}
+
+string toDot(const Heap &h) {
+    using namespace std;
+    stringstream ss;
+    ss << "digraph G {\n";
+    ss << "graph [ordering=\"out\"]\n";
+    ss << "\"" << h.at(0) << "\"\n";
+    ss << toDotImpl(h, 0);
+    ss << "}\n";
+    return ss.str();
 }
--- p157/Heap.h
+++ p157/Heap.h
@@ -9,45 +9,120 @@
 // and INT_MAX to be infinity
 #include <climits>
 
+/**
+ * Helper class to manage an array pointer.
+ * @tparam T The array type.
+ */
+template<typename T>
+class Array {
+public:
+    /**
+     * Creates an array of the specified size.
+     * @param size The size of the array.
+     */
+    explicit Array(size_t size) : val(new T[size]) {}
+
+    ~Array() { delete[] val; }
+
+    /**
+     * The pointer to the array
+     */
+    T *const val;
+
+    T &operator[](size_t index) {
+        return val[index];
+    }
+
+private:
+    //Prevent copying.
+    Array(const Array &rhs) {}
+
+    Array &operator=(const Array &rhs) {}
+};
+
 //------------------------------------------------------------
 // Heap class
 //------------------------------------------------------------
 class Heap {
- public:
-  // Constructor
-  Heap();
+public:
+    // Constructor
+    Heap();
 
-  // This constructor assumes that every element of the array is an
-  // element of the heap.
-  Heap(int* inArray, int inArraySize, int inHeapSize);
+    // This constructor assumes that every element of the array is an
+    // element of the heap.
+    Heap(int *inArray, int inArraySize, int inHeapSize);
 
-  // Destructor
-  ~Heap();
+    // Accesses an element of the array.
+    int at(int i) const;
 
-  // Accesses an element of the array.
-  int at(int i) const;
+    /**
+     * Returns the index of the left child of the node at index \p i
+     * @param i The parent node
+     * @return The left child of \p i or -1 if it doesn't have a left child.
+     */
+    int left(int i) const;
 
-  // Allows comparison between results
-  bool operator==(const Heap& rhs);
-  bool operator==(const int* rhs);
+    /**
+     * Returns whether the node at index \p i has a left child or not.
+     * @param i The parent node.
+     * @return Whether it has a left child.
+     */
+    bool hasLeft(int i) const;
 
-  // Useful for debugging. To use:
-  //   Heap h;
-  //   cout << h << endl;
-  friend std::ostream& operator<<(std::ostream& out, const Heap& h);
-  
- private:
-  // The array
-  int* A;
+    /**
+     * Returns the index of the right child of the node at index \p i
+     * @param i The parent node
+     * @return The right child of \p i or -1 if it doesn't have a right child.
+     */
+    int right(int i) const;
 
-  // Size of the array
-  int arraySize;
+    /**
+     * Returns whether the node at index \p i has a right child or not.
+     * @param i The parent node.
+     * @return Whether it has a right child.
+     */
+    bool hasRight(int i) const;
 
-  // The number of elements in the heap
-  int n;
+    /**
+     * Returns the index of the parent node of the node at index \p i
+     * @param i The child node
+     * @return The parent of \p i or -1 if the node doesn't have a parent node.
+     */
+    int parent(int i) const;
+
+    /**
+     * Bubbles down the node at index \p i
+     * @param i The node to bubble down
+     */
+    void maxHeapify(int i);
+
+    /**
+     * Builds a max heap such that each node is greater than both its left and right children.
+     */
+    void buildMaxHeap();
+
+    // Allows comparison between results
+    bool operator==(const Heap &rhs);
+
+    bool operator==(const int *rhs);
+
+    // Useful for debugging. To use:
+    //   Heap h;
+    //   cout << h << endl;
+    friend std::ostream &operator<<(std::ostream &out, const Heap &h);
+
+private:
+    // The array
+    Array<int> A;
+
+    // Size of the array
+    int arraySize;
+
+    // The number of elements in the heap
+    int n;
 };
 
 // Useful for debugging. To use:
 //   Heap h;
 //   cout << h << endl;
-std::string toDot(const Heap& h);
+std::string toDot(const Heap &h);
