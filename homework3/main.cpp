54483801d4b55a5616c9ff36b5c3b8ff49496642
Rosetta Roberts <rosettafroberts@gmail.com>
2019-02-03 17:18:31 -0700

    Finished homework 3

--- homework3/main.cpp
+++ homework3/main.cpp
@@ -6,7 +6,6 @@
 #include <sstream>
 #include <iomanip>
 #include <set>
-#include <dialog.h>
 #include "randlcg.hpp"
 
 double findMean(Randlcg rand, int count) {
@@ -31,18 +30,6 @@ std::vector<int> bin(Randlcg rand, size_t numBins, int count) {
     return bins;
 }
 
-void printHistogram(const std::vector<int> &bars, int max, const std::vector<std::string>& rowLabels) {
-    for (int i = 0; i < bars.size(); i++) {
-        int bar = bars[i];
-        int width = static_cast<int>(std::round(bar * 10000.0 / max));
-        std::cout << rowLabels[i];
-        for (int j = 0; j < width; j++) {
-            std::cout << "#";
-        }
-        std::cout << std::endl;
-    }
-}
-
 int periodOf(Randlcg r) {
     std::set<double> elementsCreated;
 
@@ -58,8 +45,6 @@ int periodOf(Randlcg r) {
 
 static const int RAND_SEED = 100001;
 
-std::vector<std::string> generateLabels();
-
 int main() {
     std::vector<int> sizes{100, 10000, 1000000, 100000000};
 
@@ -68,14 +53,13 @@ int main() {
         std::cout << "Mean of first " << size << " elements is " << findMean(rand, size) << std::endl;
     }
 
-    std::vector<std::string> labels = generateLabels();
-
     for (int size : sizes) {
         Randlcg rand{RAND_SEED};
         auto bins = bin(rand, 100, size);
 
-        std::cout << "Histogram of " << size << " elements is:" << std::endl;
-        printHistogram(bins, size, labels);
+        std::cout << "Histogram of " << size << " elements is: " << std::endl;
+        std::copy(bins.begin(), bins.end(), std::ostream_iterator<int>(std::cout, " "));
+        std::cout << std::endl;
     }
 
     std::cout << "Period is " << periodOf(Randlcg{RAND_SEED}) << std::endl;
@@ -83,15 +67,3 @@ int main() {
     return 0;
 }
 
-std::vector<std::string> generateLabels() {
-    std::vector<std::string> labels;
-
-    for (int i = 0; i < 100; i++) {
-        std::ostringstream ss;
-        ss.precision(2);
-        ss << std::fixed;
-        ss << i/100.0 << "-" << (i + 1)/100.0 << ": ";
-        labels.push_back(ss.str());
-    }
-    return labels;
-}
