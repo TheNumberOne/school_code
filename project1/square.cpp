07b33b9959dfe23524a3cc1b3e8d4fb73d532022
Rosetta Roberts <rosettafroberts@gmail.com>
2019-02-04 17:10:06 -0700

    Added initial square

--- project1/square.cpp
+++ project1/square.cpp
@@ -1,47 +1,55 @@
-///////////////////////////////////
-// square.cpp
-//
-// OpenGL program to draw a square.
-// 
-// Sumanta Guha.
-///////////////////////////////////
 
 #include <GL/glew.h>
-#include <GL/freeglut.h> 
+#include <GL/freeglut.h>
+#include <iostream>
 
-// Drawing routine.
-void drawScene(void)
+void drawScene()
 {
 	glClear(GL_COLOR_BUFFER_BIT);
+	glClear(GL_DEPTH_BUFFER_BIT);
 
-	glColor3f(0.0, 0.0, 0.0);
+	glColor3f(1.0, 0.0, 0.0);
 
-	// Draw a polygon with specified vertices.
+	glEnable(GL_DEPTH_TEST);
 	glBegin(GL_POLYGON);
-	glVertex3f(20.0, 20.0, 0.0);
-	glVertex3f(80.0, 20.0, 0.0);
-	glVertex3f(80.0, 80.0, 0.0);
-	glVertex3f(20.0, 80.0, 0.0);
+	glVertex3f(0, 0, 2);
+	glVertex3f(0, 30.f, 2);
+	glVertex3f(-30.f, 30.f, 2);
+	glVertex3f(-30.f, 0, 2);
 	glEnd();
+
+
+	glColor3f(0, 0, 1);
+	glBegin(GL_POLYGON);
+	glVertex3d(0, 0, 0);
+    glVertex3d(0, -20, 0);
+    glVertex3d(20, -20, 0);
+    glVertex3d(20, 0, 0);
+    glEnd();
+    glDisable(GL_DEPTH_TEST);
 
 	glFlush();
 }
 
 // Initialization routine.
-void setup(void)
+void setup()
 {
-	glClearColor(1.0, 1.0, 1.0, 0.0);
+	glClearColor(0.0, 0.0, 0.0, 0.0);
 }
 
 // OpenGL window reshape routine.
 void resize(int w, int h)
 {
 	glViewport(0, 0, w, h);
+	std::cout << w << " " << h << std::endl;
 
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	glOrtho(0.0, 100.0, 0.0, 100.0, -1.0, 1.0);
-	
+	glFrustum(-30.*w/h, 30.*w/h, -30, 30, 5, 20);
+	glTranslatef(0, 0, 1);
+	glRotatef(-5, 1, 1, 0);
+	glTranslatef(0, 0, -10);
+
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 }
@@ -53,7 +61,6 @@ void keyInput(unsigned char key, int x, int y)
 	{
 	case 27:
 		exit(0);
-		break;
 	default:
 		break;
 	}
@@ -64,13 +71,13 @@ int main(int argc, char **argv)
 {
 	glutInit(&argc, argv);
 
-	glutInitContextVersion(4, 3);
+	glutInitContextVersion(3, 0);
 	glutInitContextProfile(GLUT_COMPATIBILITY_PROFILE);
 
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA | GLUT_DEPTH);
 	
-	glutInitWindowSize(500, 500);
-	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(850, 600);
+	glutInitWindowPosition(0, 0);
 	
 	glutCreateWindow("square.cpp");
 	
