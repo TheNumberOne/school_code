a00620c3b67c454e52981b2b4e6790cd1da795de
Rosetta Roberts <rosettafroberts@gmail.com>
2019-02-11 17:04:26 -0700

    finished p171

--- p171/insertion.h
+++ p171/insertion.h
@@ -1,2 +1,18 @@
 #pragma once
 
+#include <utility>
+#include <cstddef>
+
+/**
+ * Inspired by the insertion sort used for small arrays of simple type
+ * in java.utils.Arrays of openjdk-8
+ */
+template <typename T>
+void insertionSort(T* items, size_t n) {
+    //Using std::swap so that minimal changes would have to be made if this was converted to a template function.
+    using std::swap;
+
+    for (size_t i = 1; i < n; i++)
+        for (size_t j = i; j > 0 && items[j-1] > items[j]; j--)
+            swap(items[j], items[j-1]);
+}
--- p171/quicksort.h
+++ p171/quicksort.h
@@ -2,3 +2,38 @@
 
 // Provides I/O
 #include <iostream>
+
+/**
+ * Partitions \p a around `a[right]` and returns the ending index of `a[right]`.
+ * @param a The array to partition
+ * @param left The index of first element to partition
+ * @param right The index of the partition.
+ * @return The ending index of the partition.
+ */
+template<typename T>
+size_t partition(T *a, size_t left, size_t right) {
+    using std::swap;
+
+    // i = beginning of partition >= a[right] rather than ending of partition < a[right]
+    size_t i = left;
+    for (size_t j = left; j < right; j++)
+        if (a[j] < a[right])
+            swap(a[j], a[i++]);
+
+    swap(a[right], a[i]);
+    return i;
+}
+
+/**
+ * Sorts \p a using quicksort.
+ * @param a The array to sort.
+ * @param size The size of \p a
+ */
+template <typename T>
+void quicksort(T *a, size_t size) {
+    if (size <= 1) return;
+
+    size_t middle = partition(a, 0, size - 1);
+    quicksort(a, middle);
+    quicksort(a + middle + 1, size - middle - 1);
+}
