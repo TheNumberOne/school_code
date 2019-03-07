0175c1f6c6614f1172d87e617fabe8d51d570e40
Rosetta Roberts <rosettafroberts@gmail.com>
2019-03-07 11:07:55 -0700

    Got everything of binary search tree completed except for remove()

--- p307/BinarySearchTree.h
+++ p307/BinarySearchTree.h
@@ -1,3 +1,4 @@
+#pragma once
 // You should start by copying your BinaryTree project into here
 // and modifying as necessary. Your BinarySearch tree will look
 // something like
@@ -9,3 +10,254 @@
 // One of the data members will be
 //
 //    GetKey _getKey;
+
+// Provides I/O
+#include <iostream>
+#include <string>
+
+#include "./util.h"
+
+/**
+ * Represents a node in a binary tree
+ */
+template<typename T>
+class BNode {
+public:
+    explicit BNode(T value) : m_data(value), m_left(nullptr), m_right(nullptr) {}
+
+    const T &data() const { return m_data; }
+
+    T &data() { return m_data; }
+
+    void setData(T data) { m_data = data; }
+
+    BNode<T> *left() const { return m_left; }
+
+    void setLeft(BNode<T> *left) { m_left = left; }
+
+    BNode<T> *right() const { return m_right; }
+
+    void setRight(BNode<T> *right) { m_right = right; }
+
+private:
+    T m_data;
+    BNode<T> *m_left;
+    BNode<T> *m_right;
+};
+
+
+/**
+ * Represents a binary search tree
+ */
+template<typename T, typename GetKey>
+class BinarySearchTree {
+public:
+    typedef decltype(std::declval<GetKey>()(std::declval<T>())) KeyType;
+
+    BinarySearchTree() = default;
+
+    // Remove copy constructs to prevent memory leaks.
+    BinarySearchTree(const BinarySearchTree<T, GetKey> &) = delete;
+
+    BinarySearchTree<T, GetKey> &operator=(const BinarySearchTree<T, GetKey>) = delete;
+
+    ~BinarySearchTree() {
+        deleteTree(_head);
+    }
+
+    /**
+     * Returns a pointer to the root node of the tree.
+     */
+    BNode<T> *head() const { return _head; }
+
+    /**
+     * Allows one to set the root node of the binary tree.
+     * Takes ownership of the node and all of its children while releasing
+     * ownership of the previous root node and children without deleting them.
+     */
+    void setHead(BNode<T> *head) { _head = head; }
+
+    /**
+     * Inserts the specified item into this BinarySearchTree
+     */
+    void insert(T item) {
+        if (_head == nullptr) {
+            setHead(new BNode<T>(item));
+            return;
+        }
+        BNode<T> *node = _head;
+        KeyType key = _getKey(item);
+        while (true) {
+            KeyType nodeKey = _getKey(node->data());
+
+            if (key < nodeKey) {
+                if (node->left() == nullptr) {
+                    node->setLeft(new BNode<T>(item));
+                    return;
+                }
+
+                node = node->left();
+            } else if (nodeKey < key) {
+                if (node->right() == nullptr) {
+                    node->setRight(new BNode<T>(item));
+                    return;
+                }
+
+                node = node->right();
+            } else {
+                node->setData(item);
+                return;
+            }
+        }
+    }
+
+    /**
+     * Returns the minimum value in this binary tree
+     */
+    const T &min() const {
+        if (_head == nullptr) throw std::runtime_error("Tree is empty");
+        BNode<T> *node = _head;
+        while (node->left() != nullptr) {
+            node = node->left();
+        }
+        return node->data();
+    }
+
+    /**
+     * Returns the maximum value in this binary tree
+     */
+    const T &max() const {
+        if (_head == nullptr) throw std::runtime_error("Tree is empty");
+        BNode<T> *node = _head;
+        while (node->right() != nullptr) {
+            node = node->right();
+        }
+        return node->data();
+    }
+
+    /**
+     * Finds the specified value under the specified root
+     */
+    BNode<T> *find(const T &value, BNode<T> *root) {
+        if (root == nullptr) return nullptr;
+        if (root->data() == value) return root;
+        BNode<T> *ret = find(value, root->left());
+        if (ret != nullptr) return ret;
+        return find(value, root->right());
+    }
+
+    /**
+     * Sees if this search tree contains the specified value. Returns false
+     * if it contains an element with the same key that is not equivalent.
+     */
+    bool contains(const KeyType &value) const {
+        return contains(_head, value);
+    }
+
+    /**
+     * Returns the item with the specified key
+     */
+    const T &get(const KeyType &key) const {
+        return get(_head, key);
+    }
+
+    /**
+     * Returns the item with the specified key
+     */
+    T &get(const KeyType &key) {
+        return const_cast<T &>(const_cast<const BinarySearchTree<T, GetKey> *>(this)->get(key));
+    }
+
+    /**
+     * Copies the elements of this tree in order to the iterator.
+     */
+    template<typename Iter>
+    void getInOrder(Iter out) const {
+        int i = 0;
+        getInOrder(_head, out);
+    }
+
+    std::string toDot() {
+        using namespace std;
+        string str = "digraph G {\n";
+        str += "graph [ordering=\"out\"]\n";
+        str += toDotNodes(_head);
+        str += toDotImpl(_head);
+        str += "}\n";
+        return str;
+    }
+
+private:
+
+    BNode<T> *_head = nullptr;
+    GetKey _getKey;
+
+    void deleteTree(BNode<T> *root) {
+        if (root == nullptr) return;
+        deleteTree(root->left());
+        deleteTree(root->right());
+        delete root;
+    }
+
+    std::string toDotImpl(BNode<T> *node) {
+        using namespace std;
+        if (!node) return "";
+        string str;
+        if (node->left()) {
+            str += dotEdge(node->data(), node->left()->data());
+        }
+        if (node->right()) {
+            str += dotEdge(node->data(), node->right()->data());
+        }
+
+        if (node->left()) {
+            str += toDotImpl(node->left());
+        }
+        if (node->right()) {
+            str += toDotImpl(node->right());
+        }
+        return str;
+    }
+
+    std::string toDotNodes(BNode<T> *node) {
+        if (node == nullptr) return "";
+        return toDotNodes(node->left()) + dotNode(node->data()) + toDotNodes(node->right());
+    }
+
+    bool contains(const BNode<T> *node, const KeyType &key) const {
+        while (true) {
+            if (node == nullptr) {
+                return false;
+            }
+
+            if (key < _getKey(node->data())) {
+                node = node->left();
+            } else if (_getKey(node->data()) < key) {
+                node = node->right();
+            } else {
+                return true;
+            }
+        }
+    }
+
+    const T &get(const BNode<T> *node, const KeyType &key) const {
+        if (node == nullptr) throw std::runtime_error("Tree doesn't contain element with specified key.");
+
+        if (key < _getKey(node->data())) {
+            return get(node->left(), key);
+        } else if (_getKey(node->data()) < key) {
+            return get(node->right(), key);
+        } else {
+            return node->data();
+        }
+    }
+
+    template<typename Iter>
+    void getInOrder(const BNode<T> *tree, Iter &out) const {
+        if (tree == nullptr) return;
+
+        getInOrder(tree->left(), out);
+        *(out++) = tree->data();
+        getInOrder(tree->right(), out);
+    }
+};
--- p307/main.cpp
+++ p307/main.cpp
@@ -53,31 +53,31 @@
 //------------------------------------------------------------
 
 #include <iostream>
-#include <sstream>
 
 #include "./util.h"
-#include "./Student.h"
-#include "./BinarySearchTree.h"
+#include "BinarySearchTree.h"
 
 using namespace std;
 
 class Identity {
- public:
-  int operator()(const int i) {
-    return i;
-  }
+public:
+    int operator()(const int i) const {
+        return i;
+    }
 };
+
 class GetId {
- public:
-  int operator()(const Student& s) {
-    return s.id();
-  }
+public:
+    int operator()(const Student &s) const {
+        return s.id();
+    }
 };
+
 class GetAge {
- public:
-  int operator()(const Student& s) {
-    return s.age();
-  }
+public:
+    int operator()(const Student &s) const {
+        return s.age();
+    }
 };
 
 //------------------------------------------------------------
@@ -85,336 +85,342 @@ class GetAge {
 //------------------------------------------------------------
 int runTests() {
 
-  int numFails = 0;
-  int testNum = 0;
+    int numFails = 0;
+    int testNum = 0;
 
-//   {
-//     BinarySearchTree<int, Identity> tree;
-//     string target = R"str(
-// digraph G {
-// graph [ordering="out"]
-// }
-// )str";
-//     TEST(stringEquals(tree.toDot(), target));
+    {
+        BinarySearchTree<int, Identity> tree;
+        string target = R"str(
+ digraph G {
+ graph [ordering="out"]
+ }
+ )str";
+        // 1
+        TEST(stringEquals(tree.toDot(), target));
 
-//     tree.insert(8);
-//     target = R"str(
-// digraph G {
-// graph [ordering="out"]
-// "8"
-// }
-// )str";
-//     TEST(stringEquals(tree.toDot(), target));
+        tree.insert(8);
+        target = R"str(
+ digraph G {
+ graph [ordering="out"]
+ "8"
+ }
+ )str";
+        // 2
+        TEST(stringEquals(tree.toDot(), target));
 
-//     tree.insert(3);
-//     target = R"str(
-// digraph G {
-// graph [ordering="out"]
-// "3"
-// "8"
-// "8" -> "3"
-// }
-// )str";
-//     TEST(stringEquals(tree.toDot(), target));
+        tree.insert(3);
+        target = R"str(
+ digraph G {
+ graph [ordering="out"]
+ "3"
+ "8"
+ "8" -> "3"
+ }
+ )str";
+        // 3
+        TEST(stringEquals(tree.toDot(), target));
 
-//     tree.insert(12);
-//     target = R"str(
-// digraph G {
-// graph [ordering="out"]
-// "3"
-// "8"
-// "12"
-// "8" -> "3"
-// "8" -> "12"
-// }
-// )str";
-//     TEST(stringEquals(tree.toDot(), target));
+        tree.insert(12);
+        target = R"str(
+ digraph G {
+ graph [ordering="out"]
+ "3"
+ "8"
+ "12"
+ "8" -> "3"
+ "8" -> "12"
+ }
+ )str";
+        // 4
+        TEST(stringEquals(tree.toDot(), target));
 
-//     tree.insert(15);
-//     target = R"str(
-// digraph G {
-// graph [ordering="out"]
-// "3"
-// "8"
-// "12"
-// "15"
-// "8" -> "3"
-// "8" -> "12"
-// "12" -> "15"
-// }
-// )str";
-//     TEST(stringEquals(tree.toDot(), target));
-//   }
+        tree.insert(15);
+        target = R"str(
+ digraph G {
+ graph [ordering="out"]
+ "3"
+ "8"
+ "12"
+ "15"
+ "8" -> "3"
+ "8" -> "12"
+ "12" -> "15"
+ }
+ )str";
+        // 5
+        TEST(stringEquals(tree.toDot(), target));
+    }
 
-//   {
-//     BinarySearchTree<int, Identity> tree;
-//     tree.insert(12);
-//     tree.insert(8);
-//     tree.insert(15);
-//     tree.insert(3);
-//     string target = R"str(
-// digraph G {
-// graph [ordering="out"]
-// "3"
-// "8"
-// "12"
-// "15"
-// "12" -> "8"
-// "12" -> "15"
-// "8" -> "3"
-// }
-// )str";
-//     TEST(stringEquals(tree.toDot(), target));
-//   }
+    {
+        BinarySearchTree<int, Identity> tree;
+        tree.insert(12);
+        tree.insert(8);
+        tree.insert(15);
+        tree.insert(3);
+        string target = R"str(
+ digraph G {
+ graph [ordering="out"]
+ "3"
+ "8"
+ "12"
+ "15"
+ "12" -> "8"
+ "12" -> "15"
+ "8" -> "3"
+ }
+ )str";
+        // 6
+        TEST(stringEquals(tree.toDot(), target));
+    }
 
-//   {
-//     BinarySearchTree<int, Identity> tree;
-//     tree.insert(3);
-//     tree.insert(8);
-//     tree.insert(15);
-//     tree.insert(12);
-//     string target = R"str(
-// digraph G {
-// graph [ordering="out"]
-// "3"
-// "8"
-// "12"
-// "15"
-// "3" -> "8"
-// "8" -> "15"
-// "15" -> "12"
-// }
-// )str";
-//     TEST(stringEquals(tree.toDot(), target));
-//   }
+    {
+        BinarySearchTree<int, Identity> tree;
+        tree.insert(3);
+        tree.insert(8);
+        tree.insert(15);
+        tree.insert(12);
+        string target = R"str(
+ digraph G {
+ graph [ordering="out"]
+ "3"
+ "8"
+ "12"
+ "15"
+ "3" -> "8"
+ "8" -> "15"
+ "15" -> "12"
+ }
+ )str";
+        TEST(stringEquals(tree.toDot(), target));
+    }
 
-//   {
-//     BinarySearchTree<int, Identity> tree;
-//     tree.insert(8);
-//     tree.insert(4);
-//     tree.insert(12);
-//     tree.insert(2);
-//     tree.insert(6);
-//     tree.insert(10);
-//     tree.insert(14);
-//     tree.insert(1);
-//     tree.insert(3);
-//     tree.insert(5);
-//     tree.insert(7);
-//     tree.insert(9);
-//     tree.insert(11);
-//     tree.insert(13);
-//     tree.insert(15);
-//     string target = R"str(
-// digraph G {
-// graph [ordering="out"]
-// "1"
-// "2"
-// "3"
-// "4"
-// "5"
-// "6"
-// "7"
-// "8"
-// "9"
-// "10"
-// "11"
-// "12"
-// "13"
-// "14"
-// "15"
-// "8" -> "4"
-// "8" -> "12"
-// "4" -> "2"
-// "4" -> "6"
-// "2" -> "1"
-// "2" -> "3"
-// "6" -> "5"
-// "6" -> "7"
-// "12" -> "10"
-// "12" -> "14"
-// "10" -> "9"
-// "10" -> "11"
-// "14" -> "13"
-// "14" -> "15"
-// }
-// )str";
-//     TEST(stringEquals(tree.toDot(), target));
-//   }
+    {
+        BinarySearchTree<int, Identity> tree;
+        tree.insert(8);
+        tree.insert(4);
+        tree.insert(12);
+        tree.insert(2);
+        tree.insert(6);
+        tree.insert(10);
+        tree.insert(14);
+        tree.insert(1);
+        tree.insert(3);
+        tree.insert(5);
+        tree.insert(7);
+        tree.insert(9);
+        tree.insert(11);
+        tree.insert(13);
+        tree.insert(15);
+        string target = R"str(
+ digraph G {
+ graph [ordering="out"]
+ "1"
+ "2"
+ "3"
+ "4"
+ "5"
+ "6"
+ "7"
+ "8"
+ "9"
+ "10"
+ "11"
+ "12"
+ "13"
+ "14"
+ "15"
+ "8" -> "4"
+ "8" -> "12"
+ "4" -> "2"
+ "4" -> "6"
+ "2" -> "1"
+ "2" -> "3"
+ "6" -> "5"
+ "6" -> "7"
+ "12" -> "10"
+ "12" -> "14"
+ "10" -> "9"
+ "10" -> "11"
+ "14" -> "13"
+ "14" -> "15"
+ }
+ )str";
+        TEST(stringEquals(tree.toDot(), target));
+    }
 
-//   {
-//     BinarySearchTree<int, Identity> tree;
-//     tree.insert(8);
-//     tree.insert(4);
-//     tree.insert(10);
-//     tree.insert(14);
-//     tree.insert(12);
-//     tree.insert(2);
-//     tree.insert(1);
-//     tree.insert(6);
-//     tree.insert(11);
-//     tree.insert(5);
-//     tree.insert(7);
-//     tree.insert(9);
-//     tree.insert(3);
-//     tree.insert(13);
-//     tree.insert(15);
-//     string target = R"str(
-// digraph G {
-// graph [ordering="out"]
-// "1"
-// "2"
-// "3"
-// "4"
-// "5"
-// "6"
-// "7"
-// "8"
-// "9"
-// "10"
-// "11"
-// "12"
-// "13"
-// "14"
-// "15"
-// "8" -> "4"
-// "8" -> "10"
-// "4" -> "2"
-// "4" -> "6"
-// "2" -> "1"
-// "2" -> "3"
-// "6" -> "5"
-// "6" -> "7"
-// "10" -> "9"
-// "10" -> "14"
-// "14" -> "12"
-// "14" -> "15"
-// "12" -> "11"
-// "12" -> "13"
-// }
-// )str";
-//     TEST(stringEquals(tree.toDot(), target));
-//   }
+    {
+        BinarySearchTree<int, Identity> tree;
+        tree.insert(8);
+        tree.insert(4);
+        tree.insert(10);
+        tree.insert(14);
+        tree.insert(12);
+        tree.insert(2);
+        tree.insert(1);
+        tree.insert(6);
+        tree.insert(11);
+        tree.insert(5);
+        tree.insert(7);
+        tree.insert(9);
+        tree.insert(3);
+        tree.insert(13);
+        tree.insert(15);
+        string target = R"str(
+ digraph G {
+ graph [ordering="out"]
+ "1"
+ "2"
+ "3"
+ "4"
+ "5"
+ "6"
+ "7"
+ "8"
+ "9"
+ "10"
+ "11"
+ "12"
+ "13"
+ "14"
+ "15"
+ "8" -> "4"
+ "8" -> "10"
+ "4" -> "2"
+ "4" -> "6"
+ "2" -> "1"
+ "2" -> "3"
+ "6" -> "5"
+ "6" -> "7"
+ "10" -> "9"
+ "10" -> "14"
+ "14" -> "12"
+ "14" -> "15"
+ "12" -> "11"
+ "12" -> "13"
+ }
+ )str";
+        TEST(stringEquals(tree.toDot(), target));
+    }
 
-//   {
-//     BinarySearchTree<int, Identity> tree;
-//     tree.insert(8);
-//     tree.insert(4);
-//     tree.insert(10);
-//     // Use the following function signature:
-//     //     T min() {
-//     //       ...
-//     //     }
-//     TEST(tree.min() == 4);
-//     TEST(tree.max() == 10);
-//   }
+    {
+        BinarySearchTree<int, Identity> tree;
+        tree.insert(8);
+        tree.insert(4);
+        tree.insert(10);
+        // Use the following function signature:
+        //     T min() {
+        //       ...
+        //     }
+        TEST(tree.min() == 4);
+        TEST(tree.max() == 10);
+    }
 
-//   {
-//     BinarySearchTree<int, Identity> tree;
-//     tree.insert(11);
-//     tree.insert(7);
-//     tree.insert(5);
-//     TEST(tree.min() == 5);
-//     TEST(tree.max() == 11);
-//   }
+    {
+        BinarySearchTree<int, Identity> tree;
+        tree.insert(11);
+        tree.insert(7);
+        tree.insert(5);
+        TEST(tree.min() == 5);
+        TEST(tree.max() == 11);
+    }
 
-//   {
-//     BinarySearchTree<int, Identity> tree;
-//     tree.insert(11);
-//     tree.insert(7);
-//     tree.insert(5);
-//     TEST(tree.contains(11));
-//     TEST(tree.contains(7));
-//     TEST(tree.contains(5));
-//     TEST(!tree.contains(1));
-//     TEST(!tree.contains(6));
+    {
+        BinarySearchTree<int, Identity> tree;
+        tree.insert(11);
+        tree.insert(7);
+        tree.insert(5);
+        TEST(tree.contains(11));
+        TEST(tree.contains(7));
+        TEST(tree.contains(5));
+        TEST(!tree.contains(1));
+        TEST(!tree.contains(6));
 
-//     TEST(tree.get(11) == 11);
-//     TEST(tree.get(7) == 7);
-//     TEST(tree.get(5) == 5);
-//   }
+        TEST(tree.get(11) == 11);
+        TEST(tree.get(7) == 7);
+        TEST(tree.get(5) == 5);
+    }
 
-//   {
-//     // Use GetId as the getKey function so that nodes
-//     // will be ordered by student ID.
-//     BinarySearchTree<Student, GetId> tree;
-//     tree.insert(Student("Joe", 587, 21));
-//     tree.insert(Student("Sally", 223, 22));
-//     tree.insert(Student("Kylie", 982, 20));
-//     tree.insert(Student("Josh", 363, 35));
-//     string target = R"str(
-// digraph G {
-// graph [ordering="out"]
-// "Sally 223 22"
-// "Josh 363 35"
-// "Joe 587 21"
-// "Kylie 982 20"
-// "Joe 587 21" -> "Sally 223 22"
-// "Joe 587 21" -> "Kylie 982 20"
-// "Sally 223 22" -> "Josh 363 35"
-// }
-// )str";
-//     TEST(stringEquals(tree.toDot(), target));
+    {
+        // Use GetId as the getKey function so that nodes
+        // will be ordered by student ID.
+        BinarySearchTree<Student, GetId> tree;
+        tree.insert(Student("Joe", 587, 21));
+        tree.insert(Student("Sally", 223, 22));
+        tree.insert(Student("Kylie", 982, 20));
+        tree.insert(Student("Josh", 363, 35));
+        string target = R"str(
+ digraph G {
+ graph [ordering="out"]
+ "Sally 223 22"
+ "Josh 363 35"
+ "Joe 587 21"
+ "Kylie 982 20"
+ "Joe 587 21" -> "Sally 223 22"
+ "Joe 587 21" -> "Kylie 982 20"
+ "Sally 223 22" -> "Josh 363 35"
+ }
+ )str";
+        TEST(stringEquals(tree.toDot(), target));
 
-//     TEST(tree.contains(587));
-//     TEST(tree.contains(223));
-//     TEST(tree.contains(982));
-//     TEST(!tree.contains(21));
-//     TEST(!tree.contains(22));
+        TEST(tree.contains(587));
+        TEST(tree.contains(223));
+        TEST(tree.contains(982));
+        TEST(!tree.contains(21));
+        TEST(!tree.contains(22));
 
-//     TEST(tree.get(587).name() == "Joe");
-//     TEST(tree.get(223).name() == "Sally");
+        TEST(tree.get(587).name() == "Joe");
+        TEST(tree.get(223).name() == "Sally");
 
-//     // Inserts elements in order into array. Students should
-//     // be ordered by student ID.
-//     Student students[4];
-//     tree.getInOrder(students);
-//     printArray(students, 4);
-//     TEST(students[0].name() == "Sally");
-//     TEST(students[1].name() == "Josh");
-//     TEST(students[2].name() == "Joe");
-//     TEST(students[3].name() == "Kylie");
-//   }
+        // Inserts elements in order into array. Students should
+        // be ordered by student ID.
+        Student students[4];
+        tree.getInOrder(students);
+        printArray(students, 4);
+        TEST(students[0].name() == "Sally");
+        TEST(students[1].name() == "Josh");
+        TEST(students[2].name() == "Joe");
+        TEST(students[3].name() == "Kylie");
+    }
 
-//   {
-//     // Use GetAge as the getKey function so that nodes
-//     // will be ordered by age.
-//     BinarySearchTree<Student, GetAge> tree;
-//     tree.insert(Student("Joe", 587, 21));
-//     tree.insert(Student("Sally", 223, 22));
-//     tree.insert(Student("Kylie", 982, 20));
-//     tree.insert(Student("Josh", 363, 35));
-//     string target = R"str(
-// digraph G {
-// graph [ordering="out"]
-// "Kylie 982 20"
-// "Joe 587 21"
-// "Sally 223 22"
-// "Josh 363 35"
-// "Joe 587 21" -> "Kylie 982 20"
-// "Joe 587 21" -> "Sally 223 22"
-// "Sally 223 22" -> "Josh 363 35"
-// }
-// )str";
-//     TEST(stringEquals(tree.toDot(), target));
+    {
+        // Use GetAge as the getKey function so that nodes
+        // will be ordered by age.
+        BinarySearchTree<Student, GetAge> tree;
+        tree.insert(Student("Joe", 587, 21));
+        tree.insert(Student("Sally", 223, 22));
+        tree.insert(Student("Kylie", 982, 20));
+        tree.insert(Student("Josh", 363, 35));
+        string target = R"str(
+ digraph G {
+ graph [ordering="out"]
+ "Kylie 982 20"
+ "Joe 587 21"
+ "Sally 223 22"
+ "Josh 363 35"
+ "Joe 587 21" -> "Kylie 982 20"
+ "Joe 587 21" -> "Sally 223 22"
+ "Sally 223 22" -> "Josh 363 35"
+ }
+ )str";
+        TEST(stringEquals(tree.toDot(), target));
 
-//     TEST(!tree.contains(587));
-//     TEST(!tree.contains(223));
-//     TEST(!tree.contains(982));
-//     TEST(tree.contains(21));
-//     TEST(tree.contains(22));
+        TEST(!tree.contains(587));
+        TEST(!tree.contains(223));
+        TEST(!tree.contains(982));
+        TEST(tree.contains(21));
+        TEST(tree.contains(22));
 
-//     TEST(tree.get(21).name() == "Joe");
-//     TEST(tree.get(22).name() == "Sally");
+        TEST(tree.get(21).name() == "Joe");
+        TEST(tree.get(22).name() == "Sally");
 
-//     Student students[4];
-//     tree.getInOrder(students);
-//     printArray(students, 4);
-//     TEST(students[0].name() == "Kylie");
-//     TEST(students[1].name() == "Joe");
-//     TEST(students[2].name() == "Sally");
-//     TEST(students[3].name() == "Josh");
-//   }
+        Student students[4];
+        tree.getInOrder(students);
+        printArray(students, 4);
+        TEST(students[0].name() == "Kylie");
+        TEST(students[1].name() == "Joe");
+        TEST(students[2].name() == "Sally");
+        TEST(students[3].name() == "Josh");
+    }
 
 //   {
 //     BinarySearchTree<int, Identity> tree;
@@ -468,13 +474,15 @@ int runTests() {
 //     TEST(stringEquals(tree.toDot(), target));
 //   }
 
-  const int numSuccesses = testNum - numFails;
-  cout << numSuccesses << "/" << testNum << " tests succeeded" << endl;
+    const int numSuccesses = testNum - numFails;
+    cout << numSuccesses << "/" << testNum << " tests succeeded" <<
+         endl;
 
-  return numFails;
+    return
+            numFails;
 }
 
 int main() {
-  return runTests();
+    return runTests();
 }
 
