37fe4b75852773a03e926aabb60127f588fd6701
Rosetta Roberts <rosettafroberts@gmail.com>
2019-04-23 00:20:53 -0600

    Got left and forwards working

--- group_project/turtle.py
+++ group_project/turtle.py
@@ -1,17 +1,16 @@
-from typing import Union
-
 import glm
+import pygame
+from OpenGL.GL import *
+from pygame import *
 
 from scene import Scene, Edge
-
-number = Union[float, int]
 
 
 class Turtle:
     def __init__(self):
-        self._pos = glm.vec3()
-        self._up = glm.vec3(0, 0, -1)
-        self._forward = glm.vec3(1, 0, 0)
+        self._pos = glm.vec4(0, 0, 0, 1)
+        self._up = glm.vec4(0, 0, 1, 1)
+        self._forward = glm.vec4(1, 0, 0, 1)
         self._scene = Scene()
 
     def forward(self, distance):
@@ -20,7 +19,10 @@ class Turtle:
         self._pos = new_pos
 
     def left(self, angle):
-        self._forward = glm.rotate(self._forward, glm.radians(angle), self._up)
+        self._forward = glm.rotate(glm.identity(glm.mat4), glm.radians(angle), self._up.xyz) * self._forward
+
+
+_the_turtle = None
 
 
 def _t():
@@ -36,3 +38,50 @@ def forward(distance):
 
 def left(distance):
     _t().left(distance)
+
+
+def aspect_ratio(size: (int, int)):
+    width, height = size
+    return width / height
+
+
+def set_display_size(size: (int, int)):
+    """Configures OpenGL and pygame to use a display with specified size."""
+    pygame.display.set_mode(size, HWSURFACE | DOUBLEBUF | OPENGL | RESIZABLE)
+    glViewport(0, 0, *size)
+
+
+def main_loop():
+    pygame.init()
+    display = (800, 600)
+    pygame.display.gl_set_attribute(GL_MULTISAMPLEBUFFERS, 1)
+    pygame.display.gl_set_attribute(GL_MULTISAMPLESAMPLES, 8)
+    pygame.display.set_caption("group project")
+    set_display_size(display)
+
+    glClearColor(1, 1, 1, 1)
+    glEnable(GL_LINE_SMOOTH)
+
+    view = glm.lookAt((0, 0, 300), (0, 0, 0), (0, 1, 0))
+    projection = glm.perspective(90, aspect_ratio(display), 10, 1000)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+            if event.type == pygame.VIDEORESIZE:
+                display = event.dict['size']
+                set_display_size(display)
+                projection = glm.perspective(90, aspect_ratio(display), 10, 1000)
+
+            if event.type == pygame.KEYUP and event.key == pygame.K_ESCAPE:
+                pygame.quit()
+                return
+
+        glClear(GL_COLOR_BUFFER_BIT)
+
+        _t()._scene.draw(view, projection)
+
+        pygame.time.wait(100)
+        pygame.display.flip()
