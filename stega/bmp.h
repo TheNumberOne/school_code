e913c56874f734e830e3aff503516258b1360a8c
Rosetta Roberts <rosettafroberts@gmail.com>
2019-04-26 03:54:35 -0600

    finished problem

--- stega/bmp.h
+++ stega/bmp.h
@@ -10,29 +10,31 @@ typedef unsigned int DWORD;
 
 #pragma pack(push, 1)
 
-typedef struct tagBITMAPFILEHEADER {
-  WORD  bfType;
-  DWORD bfSize;
-  WORD  bfReserved1;
-  WORD  bfReserved2;
-  DWORD bfOffBits;
+typedef struct tagBITMAPFILEHEADER
+{
+    WORD bfType;
+    DWORD bfSize;
+    WORD bfReserved1;
+    WORD bfReserved2;
+    DWORD bfOffBits;
 } BITMAPFILEHEADER, *PBITMAPFILEHEADER;
 
-typedef struct tagBITMAPINFOHEADER {
-  // DWORD biSize;
-  // LONG  biWidth;
-  // LONG  biHeight;
-  int biSize;
-  short  biWidth;
-  short  biHeight;
-  WORD  biPlanes;
-  WORD  biBitCount;
-  DWORD biCompression;
-  DWORD biSizeImage;
-  LONG  biXPelsPerMeter;
-  LONG  biYPelsPerMeter;
-  DWORD biClrUsed;
-  DWORD biClrImportant;
+typedef struct tagBITMAPINFOHEADER
+{
+    // DWORD biSize;
+    // LONG  biWidth;
+    // LONG  biHeight;
+    int biSize;
+    short biWidth;
+    short biHeight;
+    WORD biPlanes;
+    WORD biBitCount;
+    DWORD biCompression;
+    DWORD biSizeImage;
+    LONG biXPelsPerMeter;
+    LONG biYPelsPerMeter;
+    DWORD biClrUsed;
+    DWORD biClrImportant;
 } BITMAPINFOHEADER, *PBITMAPINFOHEADER;
 
 #pragma pack(pop)
--- stega/stega.cpp
+++ stega/stega.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <string>
 
 using namespace std;
 
@@ -15,46 +16,64 @@ size_t num_pixels;
 // The size of the header
 size_t offset;
 
-void readBmp(const string& filename);
+void readBmp(const string &filename);
 
-int main(int argc, char **argv) {
-  if (argc < 2) {
-    printf("Usage: stega filename\n");
-    return 1;
-  }
-
-  readBmp(argv[1]);
-
-  printf("Successfully read %s. Now print the secret message!\n", argv[1]);
-
-  // TODO: iterate through bytes of data in the image and reconstruct
-  // the encoded message.
-
-  // Note: buffer[offset] contains the first byte of data in the image
-
-  return 0;
+int main(int argc, char **argv)
+{
+    if (argc < 2) {
+        printf("Usage: stega filename\n");
+        return 1;
+    }
+    
+    readBmp(argv[1]);
+    
+    printf("Successfully read %s. Now print the secret message!\n", argv[1]);
+    
+    // TODO: iterate through bytes of data in the image and reconstruct
+    // the encoded message.
+    
+    std::string message(num_pixels * 3 / 8, '\0');
+    char *i = &message[0];
+    auto *start = (uint8_t *) (buffer.data() + offset);
+    uint8_t *end = start + num_pixels * 3;
+    
+    for (auto p = (uint64_t *) start; (uint8_t *) (p + 1) <= end; p++) {
+        // extract least significant bits
+        uint64_t m = *p & 0x0101010101010101uL;
+        // squash the bits together
+        uint64_t a = m | (m >> 7u);
+        uint64_t b = a | (a >> 14u);
+        uint8_t c = b | (b >> 28u);
+        if (!c) break;
+        *(i++) = c;
+    }
+    message.resize(i - message.data());
+    std::cout << message << std::endl;
+    
+    return 0;
 }
 
-void readBmp(const string& filename) {
-  std::ifstream file(filename);
-
-  PBITMAPFILEHEADER file_header;
-  PBITMAPINFOHEADER info_header;
-
-  if (file) {
-    file.seekg(0,std::ios::end);
-    streampos file_length;
-    file_length = file.tellg();
-    file.seekg(0,std::ios::beg);
-
-    buffer.resize(file_length);
-    file.read(&buffer[0], file_length);
-
-    file_header = (PBITMAPFILEHEADER)(&buffer[0]);
-    info_header = (PBITMAPINFOHEADER)(&buffer[0] + sizeof(BITMAPFILEHEADER));
-
-    num_pixels = info_header->biWidth * info_header->biHeight;
-    offset = sizeof(BITMAPFILEHEADER) + info_header->biSize;
-  }
+void readBmp(const string &filename)
+{
+    std::ifstream file(filename);
+    
+    PBITMAPFILEHEADER file_header;
+    PBITMAPINFOHEADER info_header;
+    
+    if (file) {
+        file.seekg(0, std::ios::end);
+        streampos file_length;
+        file_length = file.tellg();
+        file.seekg(0, std::ios::beg);
+        
+        buffer.resize(file_length);
+        file.read(&buffer[0], file_length);
+        
+        file_header = (PBITMAPFILEHEADER) (&buffer[0]);
+        info_header = (PBITMAPINFOHEADER) (&buffer[0] + sizeof(BITMAPFILEHEADER));
+        
+        num_pixels = info_header->biWidth * info_header->biHeight;
+        offset = sizeof(BITMAPFILEHEADER) + info_header->biSize;
+    }
 }
 
